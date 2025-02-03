59858ebf7bd369b7363cb081dc28df0815980bfb
iMythms <maythams.149@gmail.com>
2025-02-03 19:05:45 +0300

    add custom fonts and update App layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './App.css'
 
 const App = () => {
-	return <div className="text-7xl">App</div>
+	return (
+		<div className="container mx-auto">
+			<h1 className="text-7xl font-semibold">app</h1>
+		</div>
+	)
 }
 
 export default App
